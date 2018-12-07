c9c4d69744382919aad3d0cbbcda0285a25edbaf
ebadier <emmanuel.badier@gmail.com>
2018-12-07 12:55:44 +0100

    - First commit

---
+++ Scripts/MetaWearUnityRPC.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Net;
+using UnityEngine;
+
+namespace MetaWearRPC.Unity
+{
+	public class MetaWearUnityRPC : MonoBehaviour
+	{
+		/// <summary>
+		/// IP address of the server. 
+		/// 127.0.0.1 is for your own computer.
+		/// </summary>
+		public string serverAddress = "127.0.0.1";
+
+		/// <summary>
+		/// Are we trying to reconnect in case of disconnection ?
+		/// </summary>
+		public bool watchForConnection = true;
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public float watchForConnectionInterval = 5.0f;
+		private float _elapsedTime;
+
+		public MetaWearRPC_Client Client { get; private set; }
+
+		protected virtual void Awake()
+		{
+			_elapsedTime = 0.0f;
+			Client = new MetaWearRPC_Client();
+			_Connect();
+		}
+
+		protected virtual void Update()
+		{
+			_elapsedTime += Time.unscaledDeltaTime;
+			if(watchForConnection && (_elapsedTime >= watchForConnectionInterval))
+			{
+				if (!Client.IsConnected)
+				{
+					_Connect();
+				}
+				_elapsedTime = 0.0f;
+			}
+
+			// Watcher is always runnning, even in Pause.
+			//if(Input.GetKeyUp(KeyCode.Keypad0))
+			//{
+			//	Time.timeScale = 0.0f;
+			//	Debug.Log("[MetaWearUnityRPC] Pause On");
+			//}
+			//else if(Input.GetKeyUp(KeyCode.Keypad1))
+			//{
+			//	Time.timeScale = 1.0f;
+			//	Debug.Log("[MetaWearUnityRPC] Pause Off");
+			//}
+		}
+
+		protected virtual void OnApplicationQuit()
+		{
+			// Release ressources before Unity3D close.
+			Client.Disconnect();
+		}
+
+		private void _Connect()
+		{
+			try
+			{
+				Debug.Log(string.Format("[MetaWearUnityRPC] Connecting to server {0}{1}...",
+					serverAddress,
+					IPAddress.IsLoopback(IPAddress.Parse(serverAddress)) ? " (Loopback)" : ""));
+
+				Client.Connect(serverAddress);
+
+				Debug.Log(string.Format("[MetaWearUnityRPC] Client {0}connected.", Client.IsConnected ? "" : "dis"));
+			}
+			catch (Exception e)
+			{
+				Debug.LogError("[MetaWearUnityRPC] Error : " + e.Message);
+			}
+		}
+	}
+}
---
+++ Scripts/MetaWearUnityRPC_Test.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+namespace MetaWearRPC.Unity
+{
+	public sealed class MetaWearUnityRPC_Test : MetaWearUnityRPC
+	{
+		public string testBoardMac = "F6:E9:DD:B4:CF:4A";
+		private ulong _testBoardMac;
+
+		public ushort vibrationDurationMs = 1;
+		public float vibrationIntensity = 100.0f;
+
+		/// <summary>
+		/// The list of reachable MetaWear boards.
+		/// </summary>
+		//public List<string> metaWearBoards = new List<string>
+		//{
+		//	"F6:E9:DD:B4:CF:4A",
+		//	"D2:80:93:BC:8C:FD",
+		//	"DF:16:4D:D1:5D:58",
+		//	"C2:48:ED:96:3B:74"
+		//};
+
+		protected override void Awake()
+		{
+			base.Awake();
+
+			_testBoardMac = Global.MacFromString(testBoardMac);
+			Debug.Log("[MetaWearUnityRPC_Test] Board Mac address : " + _testBoardMac);
+		}
+
+		protected override void Update()
+		{
+			base.Update();
+
+			if(Input.GetKeyUp(KeyCode.Keypad0))
+			{
+				Client.CloseBoard(_testBoardMac);
+				Debug.Log("[MetaWearUnityRPC_Test] Board " + testBoardMac + " closed.");
+			}
+			else if(Input.GetKeyUp(KeyCode.Keypad1))
+			{
+				Client.InitBoard(_testBoardMac);
+				Debug.Log("[MetaWearUnityRPC_Test] Board " + testBoardMac + " initialized.");
+			}
+			else if (Input.GetKeyUp(KeyCode.Keypad2))
+			{
+				string model = Client.GetBoardModel(_testBoardMac);
+				Debug.Log("[MetaWearUnityRPC_Test] Board " + testBoardMac + " model is : " + model);
+			}
+			else if (Input.GetKeyUp(KeyCode.Keypad3))
+			{
+				byte batLevel = Client.GetBatteryLevel(_testBoardMac);
+				Debug.Log("[MetaWearUnityRPC_Test] Board " + testBoardMac + " battery left : " + batLevel);
+			}
+			else if(Input.GetKeyUp(KeyCode.Keypad4))
+			{
+				Client.StartBuzzer(_testBoardMac, vibrationDurationMs);
+				Debug.Log("[MetaWearUnityRPC_Test] Board " + testBoardMac + " buzzering for " + vibrationDurationMs + " ms");
+			}
+			else if (Input.GetKeyUp(KeyCode.Keypad5))
+			{
+				Client.StartMotor(_testBoardMac, vibrationDurationMs, vibrationIntensity);
+				Debug.Log("[MetaWearUnityRPC_Test] Board " + testBoardMac + " vibration for " + vibrationDurationMs + " ms");
+			}
+		}
+	}
+}
